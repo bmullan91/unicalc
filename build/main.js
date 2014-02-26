ad7b28c533349c957ff0a4afd02ccd848bf2b9e4
Brian Mullan <bmullan91@gmail.com>
2014-02-26 23:03:30 +0000

    caching score meter width

--- build/main.js
+++ build/main.js
@@ -287,11 +287,12 @@ module.exports = {
 },{"./calculator":2,"./domParser":3}],6:[function(require,module,exports){
 var container = document.getElementById('Score'),
 		marker = container.querySelector('.marker'),
-    value = container.querySelector('.value');
+    value = container.querySelector('.value'),
+    width = container.scrollWidth;
 
 module.exports.update = function(score) {
 	score = Math.round(score * 100) / 100;
-  var leftPos = ((container.scrollWidth / 100) * score);
+  var leftPos = ((width / 100) * score);
 
   marker.style.left =  leftPos-1.5 +'px';
   value.innerHTML = score+'%';
