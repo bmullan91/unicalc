0b5c5adb8adb7f71731a97dc3c854b60814e4ad7
Brian Mullan <bmullan91@gmail.com>
2014-03-16 00:38:42 +0000

    now using transform: translateX

--- modules/scoreMeter.js
+++ modules/scoreMeter.js
@@ -3,10 +3,18 @@ var container = document.getElementById('Score'),
     value     = container.querySelector('.value'),
     width     = container.scrollWidth;
 
+function applyTransform(value) {
+  var props = ['-webkit-transform', '-moz-transform', '-ms-transform', '-o-transform', 'transform'];
+
+  props.forEach(function(prop) {
+    marker.style[prop] = 'translateX(' + value + 'px)';
+  });
+}
+
 module.exports.update = function(score) {
 	score = Math.round(score * 100) / 100;
   var leftPos = ((width / 100) * score);
 
-  marker.style.left =  leftPos-1.5 +'px';
+  applyTransform(leftPos-1.5);
   value.innerHTML = score+'%';
 };
