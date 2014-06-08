934a9df3af2d1010de4fe5dba904d842d45e20fd
Brian Mullan <bmullan91@gmail.com>
2014-06-08 13:04:50 +0100

    adding uglyify and minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,52 @@
-var gulp       = require('gulp'), 
+var gulp       = require('gulp'),
+    clean      = require('gulp-clean'),
+    concat     = require('gulp-concat'),
+    uglify     = require('gulp-uglify'), 
+    minifyCSS  = require('gulp-minify-css'),
     browserify = require('gulp-browserify');
 
-gulp.task('browserify', function() {
+
+
+gulp.task('clean', function() {
+  return gulp.src('./dist', {read: false})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('move', ['clean'], function () {
+  gulp.src('./css/font/**')
+    .pipe(gulp.dest('./dist/font'));
+});
+
+gulp.task('minify-css', ['clean'], function() {
+  gulp.src('./css/*.css')
+    .pipe(concat('min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('browserify-uglify', ['clean'], function() {
   // Single entry point to browserify
   gulp.src('./js/main.js')
       .pipe(browserify({}))
-      .pipe(gulp.dest('./build'));
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/'));
 
   gulp.src('./test/tests.js')
       .pipe(browserify({}))
-      .pipe(gulp.dest('./build'));
+      .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('browserify', ['clean'], function() {
+  // Single entry point to browserify
+  gulp.src('./js/main.js')
+      .pipe(browserify({}))
+      .pipe(gulp.dest('./dist/'));
+
+  gulp.src('./test/tests.js')
+      .pipe(browserify({}))
+      .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['clean', 'move', 'browserify-uglify', 'minify-css']);
+
+gulp.task('debug', ['clean', 'move', 'browserify', 'minify-css'])
