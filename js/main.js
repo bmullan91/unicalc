aac9319d72097d788eb8c468d5bb8a7be03b1fd9
Brian Mullan <bmullan91@gmail.com>
2014-06-11 03:39:36 +0100

    our new main.js / mediator

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //acts as a mediator - main entry point for browserify
+var polyfills = require('./modules/polyfills')();
 var view = require('./view');
 var calculator = require('./modules/calculator');
 var validator = require('./modules/validator');
 var LS = require('./modules/localStorage');
 
 (function init() {
-
   if(LS.isAvailable()) {
     if(LS.get()) {
       view.showOpenButton(); //will intern call the hide on save button
@@ -13,27 +13,25 @@ var LS = require('./modules/localStorage');
       view.showSaveButton();
     }
   }
-
   //register click listeners
   view.setButtonListener('calculate', calculateClicked);
   view.setButtonListener('save', saveClicked);
   view.setButtonListener('open', openClicked);
   view.init();
-
 })();
 
-
 function calculateClicked() {
+  view.clearErrors();
   var data = view.getInputData();
   if(!validator.validate(data)) {
     return view.showError("Check each years <em>worth</em> percentages, something isn't quite right.");
   }
-  view.clearErrors();
   view.updateScore(calculator.calculate(data));
 }
 
 function saveClicked() {
-  LS.set(view.getInputData());
+  var data = view.prepForSave();
+  if(data.length) LS.set(data);
 }
 
 function openClicked() {
