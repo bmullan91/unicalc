b925f3dc5d8d46e6058d595e7f8bd74a60186a62
Brian Mullan <bmullan91@gmail.com>
2015-01-28 22:00:07 +0000

    fixing marker issue on mobile and pressing calculate twice when on 100%

--- js/view/scoreMeter.js
+++ js/view/scoreMeter.js
@@ -1,6 +1,8 @@
 var container = document.getElementById('Score');
+var meterContainer = container.querySelector('.meter-container');
 var marker = container.querySelector('.marker');
 var value = container.querySelector('.value');
+var markerWidth = 3; // 3px, not worth doing a window.getComputedStyle
 var cssProps = ['-webkit-transform', '-moz-transform', '-ms-transform', '-o-transform', 'transform'];
 
 function applyTransform(value) {
@@ -9,10 +11,70 @@ function applyTransform(value) {
   });
 }
 
-module.exports.update = function(score) {
-	score = Math.round(score * 100) / 100;
-  var leftPos = ((container.scrollWidth / 100) * score);
+function onMobile() {
+  var margins = ((document.body.scrollWidth - meterContainer.scrollWidth) / 2);
+  return (margins < value.scrollWidth);
+}
 
-  applyTransform(leftPos-1.5);
+/*
+  Algebra time.
+
+  <-------------- a ------------------>
+  [==========score-meter==============]
+
+    markerPos
+  [====|=====score-meter==============]
+
+
+  
+  [====|=====score-meter==============]
+    [value]
+    <--b-->
+  
+  
+  Edge case @ 98%> roughly
+
+  y = width overlapping the score-meter
+  z = width overhanging
+
+                                    <y><z> 
+  [==========score-meter=============|]
+                                   [value]
+
+  
+  y = a - markerPos
+  z = b - y
+
+  offset = -(z)
+*/
+function setMarkerValuePos(markerPos) {
+  var a = meterContainer.scrollWidth;
+  var b = value.scrollWidth;
+
+  var y = a - markerPos;
+  var z = b - y;
+
+  var offset = -(b / 2); //default
+
+  if(onMobile()) {
+    if(markerPos < (b / 2)) {
+      offset = 0;
+    } else if(z > 0) {
+      //overlap
+      offset = -(z);
+    } else {
+      //default middle ground, already set.
+    }
+  }
+
+  value.style.marginLeft = offset + 'px';
+}
+
+module.exports.update = function(score) {
+  score = Math.round(score * 100) / 100;
+  var leftPos = ((meterContainer.scrollWidth / 100) * score);
+
+  applyTransform(leftPos - (markerWidth / 2));
   value.innerHTML = score+'%';
+  setMarkerValuePos(leftPos);
 };
